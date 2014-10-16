b7fb3eb2dd972b1649f3c6f548b9235904d7953c
Abiy Seifu <abiy@ripple.com>
2014-10-16 12:37:39 -0700

    updated but for latest gatewayd-3.29.1

--- bots/record_deposits_bot.js
+++ bots/record_deposits_bot.js
@@ -2,7 +2,6 @@ var RecordDeposits = require(__dirname+'/../lib/record_deposits');
 var config = require(__dirname+'/../config/config.js');
 
 var recordDeposits = new RecordDeposits({
-  external_account_id: config.get('DESTINATION_TAG'),
   currency: config.get('CURRENCY'),
   interval: config.get('INTERVAL')
 });
